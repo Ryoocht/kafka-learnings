9052444bd141076a50701db85a439ac26b64d003
Ryochi Tanaka <ryo1.t.zubu@gmail.com>
2024-09-13 23:59:21 +1000

    Add Producer

--- kafka-docker-dotnet/KafkaDockerDotnet.ProducerApi/Program.cs
+++ kafka-docker-dotnet/KafkaDockerDotnet.ProducerApi/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KafkaDockerDotnet.ProducerApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ProducerService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/event-producing", async (ProducerService producer, CancellationToken cancellationToken) =>
+{
+
+    await producer.ProduceAsync(cancellationToken);
+
+    return "Event Sent!";
+})
+.WithOpenApi();
+
+app.Run();
